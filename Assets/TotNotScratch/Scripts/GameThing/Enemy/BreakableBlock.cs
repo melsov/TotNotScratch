6ca94bf51ce340641f57d11f8c4748f73d520fa0
melsov <melsovereign@yahoo.com>
2018-01-24 09:02:07 -0500

    Breakable block

---
+++ Assets/TotNotScratch/Scripts/GameThing/Enemy/BreakableBlock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using VctorExtensions;
+
+public class BreakableBlock : MonoBehaviour
+{
+    [SerializeField]
+    private string breakSound = "wooden-ball";
+    [SerializeField]
+    private ParticleSystem particles;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        PlayerPlatformerController pl = collision.GetComponent<PlayerPlatformerController>();
+
+        if (pl) {
+            Vector2 dif = transform.position - pl.transform.position;
+            if (true || dif.y > 0f) {
+                particles.Play();
+                ComponentHelper.FindAnywhereOrAdd<AudioManager>().play(breakSound);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
